2e943a023dd35c8c55020878975a98ff9f813576
HenriqueTrigolo <henriquetrigolo@hotmail.com>
2022-10-06 10:27:39 -0300

    middlewares para dashboard (e outras futuras)

--- controllers/PensamentoController.js
+++ controllers/PensamentoController.js
@@ -5,4 +5,8 @@ module.exports = class PensamentoController {
     static async showPensamentos(req, res){
         res.render('pensamentos/home')
     }
+
+    static async dashboard(req, res){
+        res.render('pensamentos/dashboard')
+    }
 }
--- routes/pensamentosRoutes.js
+++ routes/pensamentosRoutes.js
@@ -2,8 +2,13 @@ const express = require('express')
 const router = express.Router()
 const PensamentoController = require('../controllers/PensamentoController')
 
+//helpers
+const checkAuth = require('../helpers/auth').checkAuth
+
+
 // controller
 
+router.get('/dashboard', checkAuth, PensamentoController.dashboard)
 router.get('/', PensamentoController.showPensamentos)
 
 module.exports = router
